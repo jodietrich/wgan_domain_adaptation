5bd31d5f6a7ea9163d57181d8c9193743c6d4dbe
jdietric <jdietric@student.ethz.ch>
2017-10-19 15:21:29 +0200

    renamed experiments

--- experiments/preactivation_residual_gen_bs2_bn.py
+++ experiments/preactivation_residual_gen_bs2_bn.py
@@ -5,7 +5,7 @@ import os
 
 from experiments.standard_parameters import *
 
-experiment_name = 'residual_identity_gen_bs2_bn_std_disc_v2'
+experiment_name = 'preactivation_residual_identity_gen_bs2_bn_std_disc_v3'
 
 # Model settings
 model_handle = model_zoo.PreAct_Res_Gen_bs2_bn
--- experiments/residual_gen_bs2_bn.py
+++ experiments/residual_gen_bs2_bn.py
@@ -5,7 +5,7 @@ import os
 
 from experiments.standard_parameters import *
 
-experiment_name = 'residual_identity_gen_bs2_bn_std_disc_2_no_preactivation_v2'
+experiment_name = 'residual_identity_gen_bs2_bn_std_disc_2_v3'
 
 # Model settings
 model_handle = model_zoo.Res_Gen_bs2_bn
