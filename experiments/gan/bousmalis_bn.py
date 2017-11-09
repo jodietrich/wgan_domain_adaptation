854cb7e8847ebad4124f0a03b8a56ee12f8eaa73
jdietric <jdietric@student.ethz.ch>
2017-11-09 16:33:12 +0100

    new experiment

--- experiments/gan/bousmalis_bn.py
+++ experiments/gan/bousmalis_bn.py
@@ -1,6 +1,6 @@
 from experiments.gan.standard_parameters import *
 
-experiment_name = 'bousmalis_bn_dropout_keep0.9_no_noise_all_small_data_1e5l1_i1'
+experiment_name = 'bousmalis_bn_dropout_keep0.9_10_noise_all_small_data_1e5l1_i1'
 
 # Model settings
 batch_normalization = True
@@ -10,7 +10,7 @@ w_reg_img_dist_l1 = 1.0e5
 
 # model to use
 def generator(xs, training, scope_reuse=False, scope_name='generator'):
-    return model_zoo.bousmalis_generator(xs, training, batch_normalization, residual_blocks=4, nfilters=8, input_noise_dim=0, scope_reuse=scope_reuse, scope_name=scope_name)
+    return model_zoo.bousmalis_generator(xs, training, batch_normalization, residual_blocks=4, nfilters=8, input_noise_dim=10, scope_reuse=scope_reuse, scope_name=scope_name)
 
 def discriminator(x, training, scope_reuse=False, scope_name='discriminator'):
     return model_zoo.bousmalis_discriminator(x, training, batch_normalization, middle_layers=5, initial_filters=8, scope_reuse=scope_reuse, scope_name=scope_name)
