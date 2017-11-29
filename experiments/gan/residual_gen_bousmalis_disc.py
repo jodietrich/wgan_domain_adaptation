92d519882461d1a57dff3c32aeee53806f2cfa29
jdietric <jdietric@student.ethz.ch>
2017-11-29 13:38:43 +0100

    total residual generator with identity output

--- experiments/gan/residual_gen_bousmalis_disc.py
+++ experiments/gan/residual_gen_bousmalis_disc.py
@@ -13,13 +13,14 @@ w_reg_img_dist_l1 = 1.0e4
 # target_field_strength = 3.0  # magnetic field strength in T of pictures in the target-domain
 
 # noise settings
+# only works without input noise because of the direct input to output connection
 use_generator_input_noise = False
 
 
 # model to use
 def generator(xs, z_noise, training, scope_reuse=False, scope_name='generator'):
     return model_zoo.residual_generator(xs, z_noise=z_noise, training=training, batch_normalization=batch_normalization,
-                                         residual_blocks=4, nfilters=16, scope_reuse=scope_reuse, scope_name=scope_name)
+                                         residual_blocks=4, nfilters=8, scope_reuse=scope_reuse, scope_name=scope_name)
 
 
 def discriminator(x, training, scope_reuse=False, scope_name='discriminator'):
