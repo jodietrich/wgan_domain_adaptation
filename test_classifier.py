39e97e438d468d6f6fc512e34efeb2d3b80893a1
Jonathan Dietrich <37444074+jodietrich@users.noreply.github.com>
2018-03-19 17:50:16 +0100

    added author

--- test_classifier.py
+++ test_classifier.py
@@ -1,6 +1,10 @@
+# Authors:
+# Jonathan Dietrich
+
 # test a given classifier on either 1.5T, 3T or both
 # print f1 score, accuracy, precision and the confusion matrix
 # make it possible to test either the best validation f1 score or loss checkpoints
+
 import test_utils
 
 __author__ = 'jdietric'
